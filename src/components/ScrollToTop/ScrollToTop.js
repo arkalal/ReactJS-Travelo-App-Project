a56adef6a5828d06d2d56847da09407a1bff0f83
arkalal <arkalal.123@gmail.com>
2022-02-12 14:08:53 +0530

    scroll to top functionality developed

--- src/components/ScrollToTop/ScrollToTop.js
+++ src/components/ScrollToTop/ScrollToTop.js
@@ -5,8 +5,16 @@ const ScrollToTop = () => {
 
     const [Scroll, setScroll] = useState(false)
 
+    window.addEventListener('scroll', () => {
+        window.scrollY > 200 ? setScroll(true) : setScroll(false)
+    })
+
+    const toTop = () => {
+        window.scrollTo({ top: 0 })
+    }
+
     return (
-        <div className={Scroll ? 'scrollToTopBlock scrollToTop' : 'scrollToTopNone scrollToTop'}>
+        <div onClick={toTop} className={Scroll ? 'scrollToTopBlock scrollToTop' : 'scrollToTopNone scrollToTop'}>
             <img src="/assets/logo.png" alt="" />
         </div>
     )
