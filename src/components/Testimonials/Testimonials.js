9eb90d928ab69faf42c46436115f790b9d245923
arkalal <arkalal.123@gmail.com>
2022-02-12 11:40:31 +0530

    developed testimonials section

--- src/components/Testimonials/Testimonials.js
+++ src/components/Testimonials/Testimonials.js
@@ -3,7 +3,50 @@ import './Testimonials.css'
 
 const Testimonials = () => {
     return (
-        <div>Testimonials</div>
+        <div className='testimonialSection'>
+            <div className="testimonialTitle">
+                <h2>Happy Customers</h2>
+            </div>
+
+            <div className="testimonials">
+                <div className="testimon">
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus, molestias.</p>
+
+                    <div className="testimonialInfo">
+                        <img src="/assets/avatarImage.jpeg" alt="" />
+
+                        <div className="testimonialDetails">
+                            <h4>Kishan Seth</h4>
+                            <span>CEO - Sashan Web Solutions</span>
+                        </div>
+                    </div>
+                </div>
+                <div className="testimon">
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus, molestias.</p>
+
+                    <div className="testimonialInfo">
+                        <img src="/assets/avatarImage.jpeg" alt="" />
+
+                        <div className="testimonialDetails">
+                            <h4>Kishan Seth</h4>
+                            <span>CEO - Sashan Web Solutions</span>
+                        </div>
+                    </div>
+                </div>
+                <div className="testimon">
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus, molestias.</p>
+
+                    <div className="testimonialInfo">
+                        <img src="/assets/avatarImage.jpeg" alt="" />
+
+                        <div className="testimonialDetails">
+                            <h4>Kishan Seth</h4>
+                            <span>CEO - Sashan Web Solutions</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
