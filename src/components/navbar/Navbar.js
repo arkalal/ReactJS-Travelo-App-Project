931004173824bc650218a1d1f5a1401ab828f70b
arkalal <arkalal.123@gmail.com>
2022-02-12 17:10:17 +0530

    full development done of UI/UX more functionalities to add in future

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,7 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
+import { GiHamburgerMenu } from 'react-icons/gi'
+import { VscChromeClose } from 'react-icons/vsc'
 
 const Navbar = () => {
+
+    const [NavToggle, setNavToggle] = useState(false)
+
     return (
         <div className='navbar'>
             <div className="brand">
@@ -10,10 +15,14 @@ const Navbar = () => {
                     <h3>Travelo</h3>
                 </div>
 
-                <div className="toggle"></div>
+                <div className="toggle">
+                    {
+                        NavToggle ? <VscChromeClose onClick={() => { setNavToggle(false) }}></VscChromeClose> : <GiHamburgerMenu onClick={() => { setNavToggle(true) }}></GiHamburgerMenu>
+                    }
+                </div>
             </div>
 
-            <ul>
+            <ul className='navbarMainLinks'>
                 <li>
                     <a className='NavLiActiveA' href="#Home">Home</a>
                 </li>
@@ -27,6 +36,23 @@ const Navbar = () => {
                     <a href="#Testimonials">Testimonials</a>
                 </li>
             </ul>
+
+            <div className={NavToggle ? 'navbarResponsive navbarResponsiveMotion' : 'navbarResponsive'}>
+                <ul>
+                    <li>
+                        <a className='NavLiActiveA' href="#Home">Home</a>
+                    </li>
+                    <li>
+                        <a href="#Services">Services</a>
+                    </li>
+                    <li>
+                        <a href="#Places">Places</a>
+                    </li>
+                    <li>
+                        <a href="#Testimonials">Testimonials</a>
+                    </li>
+                </ul>
+            </div>
 
             <button>Connect</button>
         </div>
